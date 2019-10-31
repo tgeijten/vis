c1ed2ff0878fa573f00e70df006b4800d7c19f6f
Thomas Geijtenbeek <t.geijtenbeek@tudelft.nl>
2019-10-31 12:53:21 +0100

    U: plane() now explicitly calls node( nullptr )

--- include/vis/plane.h
+++ include/vis/plane.h
@@ -8,7 +8,7 @@ namespace vis
 	class VIS_API plane : public node
 	{
 	public:
-		plane() {}
+		plane() : node( nullptr ) {}
 		plane( node& parent, const vec3f& width, const vec3f& height, const xo::path& image, float wrep = 1.0f, float hrep = 1.0f );
 		plane( node& parent, int x_tiles, int z_tiles, float tile_size, color a, color b );
 	};
